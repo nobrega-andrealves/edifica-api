57f7669c4277c90ed7ddd2af8da0995057380f11
nobrega-andrealves <nobrega.andrealves@gmail.com>
2024-03-13 20:28:57 -0300

    [add] Criação do projeto com Spring Initializr (spring.start.io).
    Adicionadas, inicialmente, 3 dependências:
    * Spring Boot DevTools
    * Lombok
    * Spring Web

---
+++ src/main/java/aan/edificaapi/EdificaApiApplication.java
@@ -0,0 +1,13 @@
+package aan.edificaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdificaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdificaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/aan/edificaapi/EdificaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package aan.edificaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdificaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
